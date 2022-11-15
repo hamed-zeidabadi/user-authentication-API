b17c0550bd4dc3adcdbd0a3f1ce29fd393f41259
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-11-15 20:48:12 +0330

    add backend logic

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-//server
+//Import Libraries
+
+const express = require("express");
+const app = express();
+
+// Add Middleware
+const cors = require("cors");
+const helmet = require("helmet");
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+require("dotenv").config();
+
+// Database connection
+
+// const connectDB = require("./db").connectDB;
+// connectDB();
+
+// Add Router
+
+// const User_Route = require("./Routes/User.Router.js");
+// app.use("/api", User_Route);
+
+// Running Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is Running On ${PORT}`));
