41a223d0aba614e7bd425c0d4cbceb87f03a7ac3
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-11-16 19:40:30 +0330

    ES6 to ES :(

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,22 @@
 //Import Libraries
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import * as dotenv from "dotenv";
-import connectDB from "./db";
-import userRouter from "./Routes/User_Router";
+const express = require("express");
 const app = express();
 
-// Add Middleware1
+// Add Middleware
+const cors = require("cors");
+const helmet = require("helmet");
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-dotenv.config();
+require("dotenv").config();
 
 // Database connection
-
 const connectDB = require("./db").connectDB;
 connectDB();
 
 // Add Router
-app.use("/api", userRouter);
+const User_Route = require("./Routes/User_Router");
+app.use("/api", User_Route);
 
 // Running Server
 const PORT = process.env.PORT || 5000;
