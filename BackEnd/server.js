b32cb6e591f7b51d101f10b719e56a4a31098ea8
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-11-20 20:49:02 +0330

    Change the structure to Rest API

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -20,6 +20,14 @@ connectDB();
 const User_Route = require("./Routes/User_Router");
 app.use("/api", User_Route);
 app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/admin", express.static(path.join(__dirname, "public/admin")));
+app.use(express.static(path.join("./public")));
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
+
 // Running Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is Running On ${PORT}`));
