c5220fb11079b5f9d448d60fd39825ad286612d8
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-11-20 20:12:22 +0330

    fix bug connect to db

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 //Hard Code :)
-const DB = "mongodb://root:rpYPQkPmiFvRQXItxQTCgARJ@michael.iran.liara.ir:33510/my-app?authSource=admin&replicaSet=rs0&directConnection=true"
+const DB =
+  "mongodb://root:rpYPQkPmiFvRQXItxQTCgARJ@michael.iran.liara.ir:33510/my-app?authSource=admin&replicaSet=rs0&directConnection=true";
 
 exports.connectDB = async () => {
-
   try {
     await mongoose
-      .connect(DB, { useNewUrlParser: true })
+      .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
       .then(() => console.log("Successfully connected to the database"));
   } catch (error) {
     console.log(error);
