c71f2f0743065ea0daf7eefea65210615e0c31f8
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-11-23 18:47:54 +0330

    Final version for deployment

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //Hard Code
 const DB =
-  "mongodb://root:rpYPQkPmiFvRQXItxQTCgARJ@michael.iran.liara.ir:33510/my-app?authSource=admin&replicaSet=rs0&directConnection=true";
+  "mongodb://root:bE45MAfDRuvcYym4dbN0NqjF@esme.iran.liara.ir:30330/my-app?authSource=admin&replicaSet=rs0&directConnection=true";
 
 exports.connectDB = async () => {
   try {
